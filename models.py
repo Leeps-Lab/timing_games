9b405d0f0e8566e459838e8d671006a0084d5e10
hzcooper <zcooper10@comcast.net>
2019-02-05 20:06:09 -0800

    Added y-axis config options

--- models.py
+++ models.py
@@ -32,11 +32,15 @@ def parse_config(config_file):
             'players_per_group': int(row['players_per_group']),
             'enable_payoff_landscape': True if row['enablePayoffLandscape'] == 'TRUE' else False,
             'others_bubbles': str(row['others_bubbles']),
-            'constantA': int(row['constantA']),
-            'constantB': float(row['constantB']),
-            'constantC': float(row['constantC']),
+            'constantLambda': int(row['lambda']),
+            'gamma': float(row['gamma']),
+            'rho': float(row['rho']),
             'constantH': int(row['constantH']),
             'updateRate': int(row['updateRate']),
+            'xMin': int(row['xMin']),
+            'xMax': int(row['xMax']),
+            'yMin': int(row['yMin']),
+            'yMax': int(row['yMax']),
             'displayLower': float(row['displayLower']),
             'displayUpper': float(row['displayUpper']),
         })
@@ -78,20 +82,32 @@ class Group(DecisionGroup):
     def period_length(self):
         return parse_config(self.session.config['config_file'])[self.round_number-1]['period_length']
 
-    def constantA(self):
-        return parse_config(self.session.config['config_file'])[self.round_number-1]['constantA']
+    def constantLambda(self):
+        return parse_config(self.session.config['config_file'])[self.round_number-1]['constantLambda']
 
-    def constantB(self):
-        return parse_config(self.session.config['config_file'])[self.round_number-1]['constantB']
+    def gamma(self):
+        return parse_config(self.session.config['config_file'])[self.round_number-1]['gamma']
 
-    def constantC(self):
-        return parse_config(self.session.config['config_file'])[self.round_number-1]['constantC']
+    def rho(self):
+        return parse_config(self.session.config['config_file'])[self.round_number-1]['rho']
 
     def constantH(self):
         return parse_config(self.session.config['config_file'])[self.round_number-1]['constantH']
 
     def updateRate(self):
         return parse_config(self.session.config['config_file'])[self.round_number-1]['updateRate']
+
+    def xMin(self):
+        return parse_config(self.session.config['config_file'])[self.round_number-1]['xMin']
+
+    def xMax(self):
+        return parse_config(self.session.config['config_file'])[self.round_number-1]['xMax']
+
+    def yMin(self):
+        return parse_config(self.session.config['config_file'])[self.round_number-1]['yMin']
+
+    def yMax(self):
+        return parse_config(self.session.config['config_file'])[self.round_number-1]['yMax']
 
     def displayLower(self):
         return parse_config(self.session.config['config_file'])[self.round_number-1]['displayLower']
@@ -100,20 +116,95 @@ class Group(DecisionGroup):
         return parse_config(self.session.config['config_file'])[self.round_number-1]['displayUpper']
 
     def maxPayoff(self):
-        sliderMax = 1 + 2 * self.constantA()
+        sliderMax = 1 + 2 * self.constantLambda()
         numPlayers = len(self.get_players())
-        ux = 1 + (2 * self.constantA() * (sliderMax)/2) - ((sliderMax/2) ** 2)
-        uy = (1 - (((numPlayers/2 - 0.5)/numPlayers)/self.constantB())) * (1 + ((numPlayers/2 - 0.5)/numPlayers)/self.constantC())
+        ux = 1 + (2 * self.constantLambda() * (sliderMax)/2) - ((sliderMax/2) ** 2)
+        uy = (1 - (((numPlayers/2 - 0.5)/numPlayers)/self.gamma())) * (1 + ((numPlayers/2 - 0.5)/numPlayers)/self.rho())
         return ux * uy
+
+    def minPayoff(self):
+        numPlayers = len(self.get_players())
+        uxLeft = 1 + (2 * self.constantLambda() * self.xMin()) - (self.xMin() ** 2)
+        uxRight = 1 + (2 * self.constantLambda() * self.xMax()) - (self.xMax() ** 2)
+        uyLeft = (1 - ((0.5/numPlayers)/self.gamma()) * (1 + ((0.5/numPlayers)/self.rho())))
+        uyRight = (1 - (((numPlayers - 0.5)/numPlayers)/self.gamma())) * (1 + ((numPlayers - 0.5)/numPlayers)/self.rho())
+        left = uxLeft * uyLeft
+        right = uxRight * uyRight
+        return min(left, right)
 
 class Player(BasePlayer):
     init_decision = FloatField(null=True)
 
     def initial_decision(self):
         if not self.init_decision:
-            self.init_decision = random.random() * (parse_config(self.session.config['config_file'])[self.round_number-1]['constantA'] * 2 + 1)
+            xMax = parse_config(self.session.config['config_file'])[self.round_number-1]['xMax']
+            xMin = parse_config(self.session.config['config_file'])[self.round_number-1]['xMin']
+            self.init_decision = random.random() * (xMax - xMin) + xMin
             self.save()
         return self.init_decision
 
-    """def set_payoff(self):
-        return calc_payoff(self)"""
+"""
+    def set_payoff(self):
+        decisions = list(Event.objects.filter(
+                channel='decisions',
+                content_type=ContentType.objects.get_for_model(self.group),
+                group_pk=self.group.pk).order_by("timestamp"))
+
+        try:
+            period_start = Event.objects.get(
+                    channel='state',
+                    content_type=ContentType.objects.get_for_model(self.group),
+                    group_pk=self.group.pk,
+                    value='period_start')
+            period_end = Event.objects.get(
+                    channel='state',
+                    content_type=ContentType.objects.get_for_model(self.group),
+                    group_pk=self.group.pk,
+                    value='period_end')
+        except Event.DoesNotExist:
+            return float('nan')
+
+        constantLambda = self.group.constantLambda()
+        gamma = self.group.gamma()
+        rho = self.group.rho()
+        numPlayers = len(self.group.get_players())
+
+        self.payoff = self.get_payoff(period_start, period_end, decisions, numPlayers, constantLambda, gamma, rho)
+
+
+    def get_payoff(self, period_start, period_end, decisions, numPlayers, constantLambda, gamma, rho):
+        period_duration = period_end.timestamp - period_start.timestamp
+
+        payoff = 0
+
+        Aa = payoff_matrix[0][self.id_in_group-1]
+        Ab = payoff_matrix[1][self.id_in_group-1]
+        Ba = payoff_matrix[2][self.id_in_group-1]
+        Bb = payoff_matrix[3][self.id_in_group-1]
+
+        if self.id_in_group == 1:
+            row_player = self.participant
+            q1, q2 = self.initial_decision(), self.other_player().initial_decision()
+        else:
+            row_player = self.other_player().participant
+            q2, q1 = self.initial_decision(), self.other_player().initial_decision()
+
+        q1, q2 = 0.5, 0.5
+        for i, d in enumerate(decisions):
+            if d.participant == row_player:
+                q1 = d.value
+            else:
+                q2 = d.value
+            flow_payoff = ((Aa * q1 * q2) +
+                           (Ab * q1 * (1 - q2)) +
+                           (Ba * (1 - q1) * q2) +
+                           (Bb * (1 - q1) * (1 - q2)))
+
+            if i + 1 < len(decisions):
+                next_change_time = decisions[i + 1].timestamp
+            else:
+                next_change_time = period_end.timestamp
+            payoff += (next_change_time - d.timestamp).total_seconds() * flow_payoff
+
+        return payoff / period_duration.total_seconds()
+"""
